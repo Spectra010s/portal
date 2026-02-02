803051048199e7a898ee3691f0ae1e705c295cd5
Spectra010s <adeloyeadetayo8@gmail.com>
2026-02-02 20:29:42 +0100

    feat: use metadata to get file size in send command

--- hiverra-portal/src/commands.rs
+++ hiverra-portal/src/commands.rs
@@ -1,5 +1,5 @@
-use std::path::PathBuf;
 use clap::Subcommand;
+use std::{fs::metadata, path::PathBuf};
 
 // 2. Defining the Choices (The Enum)
 // 'pub' makes this visible to main.rs
@@ -16,20 +16,31 @@ pub enum Commands {
 impl Commands {
     // This is the method attached to the Enum
     // pub is needed here also to be able to call the function
-   pub fn execute(&self) {
+    pub fn execute(&self) {
         match self {
             Commands::Send { file } => {
                 // Check 1. check if the path exists before attempting to send
                 if !file.exists() {
                     println!("Error: The file '{}' does not exist.", file.display());
                 } else if file.is_dir() {
-               // Check 2: Is it a folder? 
-               // We don't want to "send" a folder yet because folders 
-               // need to be zipped or recursed.
-                    println!("Error: '{}' is a directory. Portal only supports single files right now.", file.display());
+                    // Check 2: Is it a folder?
+                    // We don't want to "send" a folder yet because folders
+                    // need to be zipped or recursed
+                    println!(
+                        "Error: '{}' is a directory. Portal only supports single files right now.",
+                        file.display()
+                    );
                 } else {
                     // If it exists AND it's not a directory, it's a file!
-                    println!("Portal: File found! Preparing to send '{}'...", file.display());
+                    // Get the metadata (size, permissions, etc.)
+                    let file_info = metadata(file).expect("Failed to read metadata");
+                    let size = file_info.len(); // Size in bytes
+
+                    println!(
+                        "Portal: File found!  Preparing to send '{}' ({} bytes)...",
+                        file.display(),
+                        size
+                    );
                 }
             }
             Commands::Receive => {
