d58f86117cb1085e79c029c3936609a882b7d948
Spectra010s <adeloyeadetayo8@gmail.com>
2026-02-15 00:54:45 +0100

    chore: cleanup files, use and mod imports

--- hiverra-portal/src/config.rs
+++
@@ -1,5 +0,0 @@
-pub mod list;
-pub mod models;
-pub mod set;
-pub mod setup;
-pub mod show;
--- hiverra-portal/src/config/list.rs
+++ hiverra-portal/src/config/list.rs
@@ -1,5 +1,7 @@
-use crate::config::models::PortalConfig;
-use anyhow::{Context, Result};
+use {
+    crate::config::models::PortalConfig,
+    anyhow::{Context, Result},
+};
 
 pub async fn list_config() -> Result<String> {
     // load config
