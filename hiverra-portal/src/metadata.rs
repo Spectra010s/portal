c903d095fd7042762d79ec286050ca90cf519452
Spectra010s <adeloyeadetayo8@gmail.com>
2026-02-18 14:32:01 +0100

    refactor: move description from filemetadat to transfer manifest

--- hiverra-portal/src/metadata.rs
+++ hiverra-portal/src/metadata.rs
@@ -3,13 +3,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TransferManifest {
     pub files: Vec<FileMetadata>,
-    pub total_size: u64,    
+    pub total_size: u64,
+    // description from sender to receiver
+    pub description: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileMetadata {
     pub filename: String,
     pub file_size: u64,
-    // description from sender
-    pub description: Option<String>,
 }
