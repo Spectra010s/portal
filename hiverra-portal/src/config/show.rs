e177aba382e0bd0a957fc78d44a231d1304d1147
Spectra <adeloyeadetayo8@gmail.com>
2026-03-15 15:27:00 +0100

    chore(config): add tracing logs to all config handlers

--- hiverra-portal/src/config/show.rs
+++ hiverra-portal/src/config/show.rs
@@ -1,16 +1,20 @@
 use {
     crate::config::models::PortalConfig,
     anyhow::{Context, Result},
+    tracing::{debug, trace},
 };
 
 pub async fn show_config_value(key: &str) -> Result<()> {
     // load the file
+    trace!("Attempting to retrieve config value for key: {}", key);
     let cfg = PortalConfig::load_all()
         .await
         .context("Failed to load configuration")?;
-    // match and show the value for the key
-    let new_val = cfg.get_key_value(key)?;
 
-    println!("{}: {}", key, new_val);
+    // match and show the value for the key
+    let val = cfg.get_key_value(key)?;
+    debug!("Retrieved value for {}: {}", key, val);
+
+    println!("{}: {}", key, val);
     Ok(())
 }
