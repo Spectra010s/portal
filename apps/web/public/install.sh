b01de3d37c49836e5bb3f37fed5da74a10108964
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-06-01 15:12:25 +0100

    ci(release): replace cargo-dist workflow

--- apps/web/public/install.sh
+++ apps/web/public/install.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -e
 
-if [ -n "$PREFIX" ] && echo "$PREFIX" | grep -q "com.termux"; then
-    INSTALLER_URL="https://github.com/Spectra010s/portal/releases/latest/download/hiverra-portal-android-installer.sh"
-else
-    INSTALLER_URL="https://github.com/Spectra010s/portal/releases/latest/download/hiverra-portal-installer.sh"
-fi
-
-curl -fsSL "$INSTALLER_URL" | sh
+curl -fsSL "https://github.com/Spectra010s/portal/releases/latest/download/hiverra-portal-installer.sh" | sh
